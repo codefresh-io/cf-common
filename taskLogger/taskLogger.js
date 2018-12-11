8669859e8d82be5be4cbf442d9e0730ce1094aac
Itai Gendler <itai@codefresh.io>
2018-12-11 12:18:25 +0200

    updateg

--- taskLogger/taskLogger.js
+++ taskLogger/taskLogger.js
@@ -194,6 +194,8 @@ var TaskLogger = function (jobId, baseFirebaseUrl, FirebaseLib) {
 
         } else if (resetStatus) {
             step.status = STATUS.PENDING;
+            step.firebaseRef.child('creationTimeStamp').set('');
+            step.firebaseRef.child('finishTimeStamp').set('');
             step.firebaseRef.child('status').set(step.status);
         }
 
