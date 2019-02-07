728d361206d829fe7cc09b406457bd49153e6e13
Oren Gurfinkel <oren.gurfinkel@codefresh.io>
2019-02-07 15:40:35 +0200

    remove array annotations from steps key

--- taskLogger/loggers/redisLogger.js
+++ taskLogger/loggers/redisLogger.js
@@ -128,7 +128,7 @@ class RedisLogger {
     }
     attachStep(step) {
         assert(this.jobId, 'jobId must be set');
-        const key = `${root}:${this.accountId}:${this.jobId}:steps:[${step.name}]`;
+        const key = `${root}:${this.accountId}:${this.jobId}:steps:${step.name}`;
         return this._wrapper(key, this, []);
 
     }
